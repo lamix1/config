63c9a9196669981fad42aa116ad72baab0b1ce3d
lamix1 <camilacristina328santoss@gmail.com>
2023-03-31 15:10:01 -0300

    Fazendo o exercício 6.8

--- garagem/models.py
+++ garagem/models.py
@@ -12,5 +12,47 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+        
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Acessórios"
+    
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Cores"
+
+class Modelo(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+    
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")        
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    ano = models.IntegerField(default=0,  null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+
+    def __str__(self):
+        return f"marca:{self.marca}, categoria:{self.categoria}, ano:{self.ano} cor:{self.cor}"
+    
+    class Meta:
+        verbose_name_plural = "Veículos"
+        
+
+
     
 

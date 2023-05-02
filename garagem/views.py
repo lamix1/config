cad89cf8fd58c1bcf82b8cee3b79c7ee975e7fa8
lamix1 <camilacristina328santoss@gmail.com>
2023-05-02 14:28:05 -0300

    Criando a API para Categoria

--- garagem/views.py
+++ garagem/views.py
@@ -5,6 +5,13 @@ from rest_framework.viewsets import ModelViewSet
 from garagem.models import Marca
 from config.serializers import MarcaSerializer
 
+from garagem.models import Categoria
+from config.serializers import CategoriaSerializer
+
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer

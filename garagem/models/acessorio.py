81db6a5181e51e5c3b5d408f9c3c90eb1aa9d151
lamix1 <camilacristina328santoss@gmail.com>
2023-08-22 13:13:47 -0300

    Arrumando a model acessório

--- garagem/models/acessorio.py
+++ garagem/models/acessorio.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 class Acessorio(models.Model):
+    id = models.BigAutoField(primary_key=True)
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.descricao
+        return f"{self.descricao} ({self.id})"
     
     class Meta:
+        verbose_name = "Acessório" 
         verbose_name_plural = "Acessórios"

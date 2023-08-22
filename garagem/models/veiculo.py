7de44328b6c262e3c1ccf2deafcb79892169bacd
lamix1 <camilacristina328santoss@gmail.com>
2023-08-22 13:04:44 -0300

    Arrumando a model veiculo

--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -5,24 +5,18 @@ from uploader.models import Image
 from garagem.models import Marca, Modelo, Categoria, Cor, Acessorio
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
-    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")        
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+    id = models.BigAutoField(primary_key=True)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(default=0,  null=True)
+    descricao = models.CharField(max_length=100)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
     acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
-    capa = models.ForeignKey(
-        Image,
-        related_name="+",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        default=None,
-    )
+    capa = models.ManyToManyField(Image, related_name="+", null=True, blank=True, default=None,)
     
     def __str__(self):
-        return f"marca:{self.marca}, categoria:{self.categoria}, ano:{self.ano} cor:{self.cor}"
+        return f"{self.modelo}- {self.ano}"
     
     class Meta:
+        verbose_name = "Veículo"
         verbose_name_plural = "Veículos"
--- garagem/serializers/veiculo.py
+++ garagem/serializers/veiculo.py
@@ -29,4 +29,4 @@ class VeiculoDetailSerializer(ModelSerializer):
 class VeiculoListSerializer(ModelSerializer):
     class Meta:
         model = Veiculo
-        fields = ["id", "modelo", "ano"]
+        fields = ["id", "modelo", "cor"]
